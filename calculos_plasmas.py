a7f608378997cfb656984661fca1e2769d6b25cb
davidvereb <ocsivereb@gmail.com>
2026-05-07 10:03:43 +0100

    Los dos primeros resultados de prueba son satisfactorios

--- calculos_plasmas.py
+++ calculos_plasmas.py
@@ -1,5 +1,5 @@
 import numpy as np
-import scipy.linalg as la
+import scipy as sp
 
 from scipy.constants import physical_constants, k, epsilon_0, e, h, m_e, m_p, c, hbar
 
@@ -20,16 +20,37 @@ class Plasma:
         self.L = plasma_length
 
         # Discretización atómica
-        self.max_radius = 350
-        self.N = 200_000
-        self.r = np.linspace(0, self.max_radius, self.N + 1)
+        self.N_r = 200_000
+        self.max_radius = 500
+        self.r = np.linspace(0, self.max_radius, self.N_r + 1)
         self.dr = self.r[1] - self.r[0]
 
         # Autoenergías y autofunciones
+        self.n_max = 10 # Nos restringimos a n=2,...,10
+        self.l_max = 3 # Nos restringimos a l=0,...3
         self.E_0 = None
         self.u_0 = None
         self.E_l = {}
         self.u_l = {}
+
+        # Transiciones y coeficientes de Einstein
+        self.nu_if = None
+        self.A_if = None
+
+        # Malla de frecuencias
+        self.N_nu = int(1e5)
+        self.nu_min = None
+        self.nu_max = None
+        self.nu = None
+
+        # Poblaciones
+        self.lambda_e = None
+        self.G_H = None
+        self.n_H = None
+        self.n_P = None
+
+        # Espectro
+        self.I = None
 
     def set_temperature(self, temperature, units='K'):
         """
@@ -65,8 +86,8 @@ class Plasma:
         :param num_intervals: Número de intervalos de la discretización radial atómica.
         :return:
         """
-        self.N = int(num_intervals)
-        self.r = np.linspace(0, self.max_radius, self.N + 1)
+        self.N_r = int(num_intervals)
+        self.r = np.linspace(0, self.max_radius, self.N_r + 1)
         self.dr = self.r[1] - self.r[0]
 
     def analitical_schrodinger(self):
@@ -79,55 +100,172 @@ class Plasma:
         caso contrario, no.
         """
         # Constantes con dimensión
-        r_D_si = np.sqrt(epsilon_0 * k * self.T_e / (self.n_e * e ** 2))  # Longitud de Debye
+        r_D_si = np.sqrt(epsilon_0 * k * self.T_e / (self.n_e * e**2))  # Longitud de Debye
         r_0_si = np.cbrt(3 / (4 * np.pi * self.n_e))  # Radio de la esfera de neutralidad
 
         # Constantes adimensionales
         r_D = r_D_si / a_0  # Longitud de Debye adimensional
         kT_e = k * self.T_e / E_h  # ¿Energía térmica electrónica? Adimensional
         r_0 = r_0_si / a_0  # Radio de la esfera de neutralidad adimensional
-        Gamma = self.Z ** 2 / (r_0 * kT_e)  # Parámetro de acoplamiento adimensional
+        Gamma = self.Z**2 / (r_0 * kT_e)  # Parámetro de acoplamiento adimensional
 
         # Potencial adimensional
         if plasma_effects:
-            V = lambda _r: -self.Z / _r * np.exp(-_r / r_D) * (1 - Gamma + 1 / 2 * Gamma * _r / r_D)
+            #V = lambda _r: -self.Z / _r * np.exp(-_r / r_D) * (1 - Gamma + 0.5 * Gamma * _r / r_D)
+            factor = Gamma / (Gamma + 1)
+            V = lambda _r: -self.Z / _r * np.exp(-_r / r_D) * (1 - factor + factor * (1 - _r / r_D) ** 2)
         else:
             V = lambda _r: -self.Z / _r
 
         # Hamiltoniano
         r_inner = self.r[1:-1]
-        first_diagonal = lambda _l: 1 / self.dr ** 2 + V(r_inner) + _l * (_l + 1) / (2 * r_inner ** 2)
-        second_diagonal = -1 / (2 * self.dr ** 2) * np.ones(self.N - 2)
+        first_diagonal = lambda _l: 1 / self.dr**2 + V(r_inner) + _l * (_l + 1) / (2 * r_inner**2)
+        second_diagonal = -1 / (2 * self.dr**2) * np.ones(self.N_r - 2)
 
         # Cálculo del espectro
-        lower_bound = -0.5 * self.Z ** 2 - 0.5
+        lower_bound = -0.5 * self.Z**2 - 0.5
         E_dict = {}
         u_dict = {}
-        n_max = 10  # Nos restringimos a n=2,...,10
-        l_max = 3 # Nos restringimos a l=0,...3
-        for l in range(l_max+1):
-            E, u_inner = la.eigh_tridiagonal(first_diagonal(l), second_diagonal, select='v',
-                                             select_range=(lower_bound, -1e-6))
-            u = np.zeros((self.N + 1, len(E)))
+        for l in range(self.l_max+1):
+            E, u_inner = sp.linalg.eigh_tridiagonal(first_diagonal(l), second_diagonal, select='v',
+                                             select_range=(lower_bound, -1e-10))
+            u = np.zeros((self.N_r + 1, len(E)))
             u[1:-1, :] = u_inner
             E_dict[l] = E
             u_dict[l] = u
 
             # Filtrado
-            num_states_i = min(n_max - l, len(E_dict[l]))
+            num_states_i = min(self.n_max - l, len(E_dict[l]))
             E_dict[l] = E_dict[l][:num_states_i]
             u_dict[l] = u_dict[l][:, :num_states_i]
 
             # Normalización
-            norm = np.sqrt(np.trapezoid(np.abs(u_dict[l]) ** 2, x=self.r, axis=0))
+            norm = np.sqrt(np.trapezoid(np.abs(u_dict[l])**2, x=self.r, axis=0))
             u_dict[l] /= norm
 
-        # Definimos autoenergías y autofunciones del plasma
-        self.E_0 = E_h*E_dict[0][0]
-        self.u_0 = E_h*u_dict[0][:, 0]
+        # Definimos autoenergías y autofunciones del plasma en Hartrees
+        if len(E_dict[0]) > 0:
+            self.E_0 = E_h * E_dict[0][0]
+            self.u_0 = E_h * u_dict[0][:, 0]
 
-        self.E_l[0] = E_h * E_dict[0][1:]
-        self.u_l[0] = E_h * u_dict[0][:, 1:]
-        for l in range(1, l_max+1):
-            self.E_l[l] = E_h * E_dict[l]
-            self.u_l[l] = E_h * u_dict[l]
+            self.E_l[0] = E_h * E_dict[0][1:]
+            self.u_l[0] = E_h * u_dict[0][:, 1:]
+            for l in range(1, self.l_max + 1):
+                self.E_l[l] = E_h * E_dict[l]
+                self.u_l[l] = E_h * u_dict[l]
+        else:
+            print("¡Aviso! El plasma es tan denso que no existen estados ligados.")
+            self.E_0 = None
+            self.u_0 = None
+
+            self.E_l[0] = []
+            self.u_l[0] = []
+            for l in range(1, self.l_max + 1):
+                self.E_l[l] = []
+                self.u_l[l] = []
+
+    def solve_transitions(self):
+        # Elementos matriz transiciones (angular)
+        x, w = np.polynomial.legendre.leggauss(20)
+
+        # Estamos estudiando la serie Lyman (n_f = 1)
+        l_f = 0
+        m_f = 0
+
+        # Regla de selección
+        l_i = 1
+
+        factor_angular = 0
+
+        for q in [-1, 0, 1]:
+            # Regla de selección
+            m_i = -q
+
+            # Polinomios de Legendre
+            P_final = sp.special.lpmv(m_f, l_f, x)
+            P_foton = sp.special.lpmv(q, 1, x)
+            P_inicial = sp.special.lpmv(m_i, l_i, x)
+
+            integral_theta = np.sum(w * P_final * P_foton * P_inicial)
+            integral_phi = 2*np.pi
+            factor_angular += (integral_theta * integral_phi)**2
+
+        # Elementos de matriz al cuadrado: S_if
+        E_f = self.E_0
+        u_f = self.u_0
+        E_i = self.E_l[1]
+        u_i = self.u_l[1]
+
+        S_if = np.zeros(len(E_i))
+        for n in range(len(E_i)):
+            integral_radial = np.trapezoid(np.conj(u_f) * self.r * u_i[:, n], x = self.r)
+            S_if[n] = integral_radial**2 * factor_angular
+
+        # Usamos Sistema Internacional a partir de ahora.
+        self.nu_if = (E_i - E_f) * E_h / h
+        self.A_if = 8 * np.pi**2 * (a_0 * e)**2 / (3 * epsilon_0 * hbar * c**3) * self.nu_if**3 * S_if
+
+    def solve_population(self, plasma_effects=True):
+        # Función de partición
+        self.G_H = 2 # Incluimos el estado fundamental n=1, l=0
+        if plasma_effects:
+            for l in range(self.l_max+1):
+                g_l = 2 * (2 * l + 1)
+                for E_nl in self.E_l[l]:
+                    self.G_H += g_l * np.exp(-(E_nl-self.E_0) / (k * self.T_e))
+        else:
+            for n in range(2, len(self.E_l[0])+2):
+                g_n = 2 * n**2
+                E_n = self.E_l[0][n-2]
+                self.G_H += g_n * np.exp(-(E_n-self.E_0) / (k * self.T_e))
+
+        # Energía potencial de ionización
+        chi_H = -self.E_0
+
+        # Longitud de onda térmica
+        self.lambda_e = np.sqrt(h**2 / (2 * np.pi * m_e * k * self.T_e))
+
+        # Población neutra (ecuación de Saha)
+        C = 2 / (self.lambda_e**3 * self.n_e * self.G_H)* np.exp(-chi_H / (k * self.T_e))
+        self.n_H = self.n_e / ((C + 1) * self.Z - 1)
+
+        # Población núcleos
+        self.n_P = (self.n_e - (self.Z - 1) * self.n_H) / self.Z
+
+
+    def solve_spectrum(self):
+        # Parámetros perfil de linea
+        sigma = 2 * np.log(2) * np.sqrt(k * self.T_e / (m_p * c ** 2)) * self.nu_if
+        n_i = np.arange(2, 2 + len(self.nu_if))
+        gamma_L = 8 * np.pi**2 * self.n_e / (6 * np.sqrt(3)) * (hbar / m_e)**2 * np.sqrt(2 * m_e /
+                (np.pi * k * self.T_e)) * (0.9 - 1.1 / self.Z) * (3 * n_i / (2 * self.Z))**2 * (n_i**2 - 3)
+        # ¿Guion puede estar mal?
+        gamma_L = np.abs(gamma_L)
+
+        # Malla frecuencias
+        self.nu_min = self.nu_if[0] * 0.95
+        self.nu_max = self.nu_if[-1] * 1.05
+        self.nu = np.linspace(self.nu_min, self.nu_max, self.N_nu)
+
+        # Perfil de línea
+        phi_V = []
+        for n in range(len(self.nu_if)):
+            perfil = sp.special.voigt_profile(self.nu - self.nu_if[n], sigma[n], gamma_L[n]/2)
+            phi_V.append(perfil)
+
+        # Intensidad específica
+        n_H_l1 = 6 * self.n_H / self.G_H * np.exp(-(self.E_l[1] - self.E_0) / (k * self.T_e))
+
+        self.I = 0
+        for n in range(len(self.nu_if)):
+            self.I += self.L * h / (4 * np.pi) * self.nu_if[n] * n_H_l1[n] * self.A_if[n] * phi_V[n]
+
+    def solve_plasma(self, plasma_effects=True):
+        print("Resolviendo ecuación de autovalores...")
+        self.solve_schrodinger(plasma_effects=True)
+        print("Resolviendo transiciones...")
+        self.solve_transitions()
+        print("Resolviendo poblaciones...")
+        self.solve_population(plasma_effects=True)
+        print("Resolviendo espectro...")
+        self.solve_spectrum()
